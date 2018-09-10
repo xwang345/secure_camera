7e01b362b77e11287d091a6f604f92761ce9e0d8
Garrik-Liu <garrikliu@Gmail.com>
2018-09-10 11:43:05 -0400

    update

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-var exec = require('child_process').exec;
-var five = require('johnny-five');
-var Raspi = require('raspi-io');
-var board = new five.Board({
+const express = require('express');
+const exec = require('child_process').exec;
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
+const request = require('request');
+
+const app = express();
+const board = new five.Board({
     io: new Raspi()
 });
 
 board.on("ready", function() {
-    var num = 0;
-    var takeP = setInterval(function() {
-        takePicture(num);
-        num++;
-    }, 500);
 
-    setTimeout(function() {
-        clearInterval(takeP);
-    }, 10000);
+    takePicture('123');
+
+    app.listen('8080', () => {
+        console.log('Device is running on port:8080');
+    });
 });
+
 
 function takePicture(name) {
     exec("fswebcam " + name + ".jpg", (err, stdout, stderr) => {
-        console.log('Success!');
+        console.log('Taking Picture Successful!');
+        postPicture(name);
     })
+
+}
+
+function postPicture(name) {
+    var formData = {
+        // Pass a simple key-value pair
+        my_field: name,
+        // Pass data via Buffers
+        my_buffer: Buffer.from([1, 2, 3]),
+        // Pass data via Streams
+        my_file: fs.createReadStream(__dirname + '/canera_imgs/' + name + '.jpg'),
+        // Pass multiple values /w an Array
+        attachments: [],
+    };
+
+    request.post({ url: 'http://localhost:8081/uploadImg', formData: formData }, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        console.log('Upload successful!  Server responded with:', body);
+    });
 }
