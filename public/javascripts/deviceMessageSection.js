fb8fc5a9bbae0a1e6ec3292fe0b8c077b846aa22
Garrik-Liu <garrikliu@Gmail.com>
2018-11-01 15:44:04 -0400

    update

--- public/javascripts/deviceMessageSection.js
+++ public/javascripts/deviceMessageSection.js
@@ -1,6 +1,8 @@
 var socket = io();
 var messageList = document.getElementById('deviceMessageList');
 
+console.log('1234');
+
 socket.on('receive image', function(imgUrl) {
     var newImg = document.createElement('img');
     newImg.setAttribute('href', imgUrl);
