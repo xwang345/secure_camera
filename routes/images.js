e32ba8c2cf712ce8d219ce65249d9365feaa4f68
Garrik-Liu <garrikliu@Gmail.com>
2018-11-02 00:00:27 -0400

    update

--- routes/images.js
+++ routes/images.js
@@ -27,7 +27,7 @@ router.socket = function(socket) {
                 next(err);
                 return;
             }
-            res.redirect('/')
+            res.redirect(`${req.baseUrl}/loadAll`)
 
         });
     });
