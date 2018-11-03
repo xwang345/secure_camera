9152ff0a7ce53f71a6c042197e3e4691b40103e2
Garrik-Liu <garrikliu@Gmail.com>
2018-11-03 02:54:15 -0400

    update

--- routes/images.js
+++ routes/images.js
@@ -7,41 +7,43 @@ var router = express.Router();
 
 router.socket = function(socket, io) {
     ioListener.initialize(socket, io);
+
+
+    // add a image to google cloud storage
+    router.post('/add', myImages.multer.single('image'), myImages.sendUploadToGCS, (req, res, next) => {
+        let data = req.body;
+
+        data.time = new Date();
+
+        if (req.file && req.file.cloudStoragePublicUrl) {
+            data.imageUrl = req.file.cloudStoragePublicUrl;
+        }
+
+        // Save the data to the database.
+        modelDatastore.create(data, (err, savedData) => {
+            if (err) {
+                next(err);
+                return;
+            }
+
+            res.redirect(`${req.baseUrl}/loadAll`)
+
+        });
+    });
+
+    router.get('/loadAll', (req, res, next) => {
+        modelDatastore.list((err, entities) => {
+            if (err) {
+                next(err);
+                return;
+            }
+
+            io.emit('reload images', entities);
+
+            res.redirect('/uploadImg');
+        })
+    });
 }
 
-// add a image to google cloud storage
-router.post('/add', myImages.multer.single('image'), myImages.sendUploadToGCS, (req, res, next) => {
-    let data = req.body;
-
-    data.time = new Date();
-
-    if (req.file && req.file.cloudStoragePublicUrl) {
-        data.imageUrl = req.file.cloudStoragePublicUrl;
-    }
-
-    // Save the data to the database.
-    modelDatastore.create(data, (err, savedData) => {
-        if (err) {
-            next(err);
-            return;
-        }
-
-        res.redirect(`${req.baseUrl}/loadAll`)
-
-    });
-});
-
-router.get('/loadAll', (req, res, next) => {
-    modelDatastore.list((err, entities) => {
-        if (err) {
-            next(err);
-            return;
-        }
-
-        io.emit('reload images', entities);
-
-        res.redirect('/uploadImg');
-    })
-});
 
 module.exports = router;
