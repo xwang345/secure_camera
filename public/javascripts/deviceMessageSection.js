a7d2b5a4b6f3174d452da862edcf5062a2b68bee
Garrik-Liu <garrikliu@Gmail.com>
2018-11-03 03:19:48 -0400

    update

--- public/javascripts/deviceMessageSection.js
+++ public/javascripts/deviceMessageSection.js
@@ -4,9 +4,12 @@ var messageList = document.getElementById('deviceMesBox');
 socket.on('reload images', function(entities) {
     console.log(123);
     entities.forEach(e => {
-        let imgElement = document.createElement('img')
-        imgElement.setAttribute('src', e.imageUrl);
-        imgElement.classList.add('deviceMessage__image')
-        messageList.appendChild(imgElement);
+        var imgElement = `
+            <div class="deviceMesBox__imgElement">
+                <span>${e.time}</span>
+                <img src=${e.imageUrl} alt="Snapshot" />
+            </div>
+        `;
+        messageList.innerHTML += imgElement;
     })
 })
