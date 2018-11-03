a4bb8122f4b490c0bfd041cc5c17bb654caf0357
Garrik-Liu <garrikliu@Gmail.com>
2018-11-03 09:23:40 -0400

    update

--- public/javascripts/deviceMessageSection.js
+++ public/javascripts/deviceMessageSection.js
@@ -1,14 +1,29 @@
-var socket = io();
-var messageList = document.getElementById('deviceMesBox');
+$(document).ready(function() {
+    var socket = io();
 
-socket.on('reload images', function(entities) {
-    entities.forEach(e => {
-        var imgElement = `
-            <div class="deviceMesBox__imgElement">
-                <span class="deviceMesBox__imgTime">${e.time}</span>
-                <img src=${e.imageUrl} alt="Snapshot" />
-            </div>
-        `;
-        messageList.innerHTML += imgElement;
+    socket.on('reload images', function(entities) {
+        var content = '';
+
+        entities.forEach(e => {
+            var check = false;
+            $('.deviceMesBox__imgElement').each(function() {
+                if ($(this).attr('src') === e.imageUrl) {
+                    check = true;
+                }
+            })
+
+            if (!check) {
+                var imgElement = `
+                <div class="deviceMesBox__imgElement">
+                    <span class="deviceMesBox__imgTime">${e.time}</span>
+                    <img src=${e.imageUrl} alt="Snapshot" />
+                </div>
+            `;
+
+                content += imgElement;
+            }
+        });
+
+        $("#deviceMesBox").html(content);
     })
-})
+});
