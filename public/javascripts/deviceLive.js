6271d9350fcafd22318f51106247002d50083d88
Garrik-Liu <garrikliu@Gmail.com>
2018-11-05 16:54:45 -0500

    update

--- public/javascripts/deviceLive.js
+++ public/javascripts/deviceLive.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var canvas = document.getElementById('video-canvas');
-    var url = 'ws://' + document.location.hostname;
+    var url = 'ws://35.243.158.28:3000';
     var player = new JSMpeg.Player(url, { canvas: canvas });
 
     console.log(url);
