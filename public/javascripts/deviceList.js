cd8db4a659216ca00f240c607cb89723ab983a12
Garrik-Liu <garrikliu@Gmail.com>
2018-11-06 00:01:22 -0500

    update

--- public/javascripts/deviceList.js
+++ public/javascripts/deviceList.js
@@ -15,10 +15,12 @@ $(document).ready(function() {
     function toggleSideColumn(sideColumn, state) {
         if (state === 'open') {
             console.log('open');
+            console.log(sideColumn.classList)
             sideColumn.toggleClass('deviceMesBox__container--close')
                 .toggleClass('deviceMesBox__container');
         } else if (state === 'close') {
             console.log('close');
+            console.log(sideColumn.classList)
             sideColumn.toggleClass('deviceMesBox__container')
                 .toggleClass('deviceMesBox__container--close');
         }
