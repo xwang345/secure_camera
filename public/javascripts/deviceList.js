7d43c69332f770dd6fd81c1b1b9df55ac5dad323
Garrik-Liu <garrikliu@Gmail.com>
2018-11-06 01:28:38 -0500

    update

--- public/javascripts/deviceList.js
+++ public/javascripts/deviceList.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $('.deviceList__deviceBtn').each(function(index) {
         $(this).click(function() {
             toggleSideColumn(deviceMesBox, 'open');
+            showMesCount('#deviceMesBox', '#deviceBtnMesCount');
         })
     })
 
@@ -20,5 +21,16 @@ $(document).ready(function() {
             sideColumn.toggleClass('deviceMesBox__container')
                 .toggleClass('deviceMesBox__container--close');
         }
+    }
+
+    function showMesCount(mesBoxId, mesCountId) {
+        let mesBox = $(mesBoxId);
+        let mesCount = $(mesCountId);
+
+        if (mesBox.attr('width') > 0) {
+            mesCount.css('opacity', 0);
+        }
+
+
     }
 });
--- public/javascripts/deviceMesBox.js
+++ public/javascripts/deviceMesBox.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 
     socket.on('reload images', function(entities) {
         loadImages(entities);
-        showMesCount('#deviceMesBox', '#deviceBtnMesCount')
     });
 
     socket.on('load images', function(entities) {
@@ -28,14 +27,4 @@ $(document).ready(function() {
         $(".deviceMesBox__mesWindow:first").html(content);
     }
 
-    function showMesCount(mesBoxId, mesCountId) {
-        let mesBox = $(mesBoxId);
-        let mesCount = $(mesCountId);
-
-        if (mesBox.attr('width') > 0) {
-            mesCount.css('opacity', 0);
-        }
-
-
-    }
 });
