d033bea8088109307fad74f956528cc376d16845
Xiaochen Wang <ryan.wang23@hotmail.com>
2018-11-12 02:53:43 -0500

    add cookie

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.setSocketIo = function(socket, io) {
     imagesRouter.setSocketIo(socket, io);
 }
 
+const user = {
+    username: ""
+  };
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -28,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static('node_modules'));
 
 app.use('/', indexRouter);
 app.use('/images', imagesRouter);
@@ -38,7 +42,12 @@ app.use(function(req, res, next) {
     next(createError(404));
 });
 
-
+app.use(clientSessions({
+    cookieName: 'session', // cookie name dictates the key name added to the request object
+    secret: 'home_security', // should be a large unguessable string
+    duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+    activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+  }));
 
 // error handler
 app.use(function(err, req, res, next) {
