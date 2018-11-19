fd1cbaad8625da1cb900950df87e3399f3a5e5f1
Garrik-Liu <garrikliu@Gmail.com>
2018-11-19 13:10:41 -0500

    update

--- routes/images.js
+++ routes/images.js
@@ -7,84 +7,76 @@ const router = express.Router();
 const KIND = "Snapshot";
 
 router.setSocketIo = function(socket, io) {
-  router.io = io;
-  router.socket = socket;
+    router.io = io;
+    router.socket = socket;
 };
 
 // add a image to google cloud storage
 router.post(
-  "/add",
-  myImages.multer.single("image"),
-  myImages.sendUploadToGCS,
-  (req, res, next) => {
-    let data = req.body;
+    "/add",
+    myImages.multer.single("image"),
+    myImages.sendUploadToGCS,
+    (req, res, next) => {
+        let data = req.body;
 
-    data.time = new Date();
+        data.time = new Date();
 
-    if (req.file && req.file.cloudStoragePublicUrl) {
-      data.imageUrl = req.file.cloudStoragePublicUrl;
+        if (req.file && req.file.cloudStoragePublicUrl) {
+            data.imageUrl = req.file.cloudStoragePublicUrl;
+        }
+
+        // Save the data to the database.
+        modelDatastore.create(KIND, data, (err, savedData) => {
+            if (err) {
+                next(err);
+                return;
+            }
+
+            modelDatastore.list(KIND, (err, entities) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+
+                if (router.io) {
+                    console.log("reload images");
+                    router.io.emit("reload images", entities);
+                    res.json({
+                        result: "success"
+                    });
+                } else {
+                    res.json({
+                        result: "fail"
+                    });
+                }
+
+                res.end();
+            }, 10);
+        });
     }
-
-    // Save the data to the database.
-    modelDatastore.create(KIND, data, (err, savedData) => {
-      if (err) {
-        next(err);
-        return;
-      }
-
-      modelDatastore.list(
-        KIND,
-        (err, entities) => {
-          if (err) {
-            next(err);
-            return;
-          }
-
-          if (router.io) {
-            console.log("reload images");
-            router.io.emit("reload images", entities);
-            res.json({
-              result: "success"
-            });
-          } else {
-            res.json({
-              result: "fail"
-            });
-          }
-
-          res.end();
-        },
-        10
-      );
-    });
-  }
 );
 
 router.get("/loadAll", (req, res, next) => {
-  modelDatastore.list(
-    KIND,
-    (err, entities) => {
-      if (err) {
-        next(err);
-        return;
-      }
+    modelDatastore.list(KIND, (err, entities) => {
+        if (err) {
+            next(err);
+            return;
+        }
 
-      if (router.io) {
-        console.log("reload images");
-        router.io.emit("reload images", entities);
-        res.json({
-          result: "success"
-        });
-      } else {
-        res.json({
-          result: "fail"
-        });
-      }
+        if (router.io) {
+            console.log("reload images");
+            router.io.emit("reload images", entities);
+            res.json({
+                result: "success"
+            });
+        } else {
+            res.json({
+                result: "fail"
+            });
+        }
 
-      res.end();
-    },
-    10
-  );
+        res.end();
+    }, 10);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,72 +3,72 @@ var router = express.Router();
 var loginDataAuth = require("../lib/loginDataAuth");
 
 router.setSocketIo = (socket, io) => {
-  router.io = io;
-  router.socket = socket;
+    router.io = io;
+    router.socket = socket;
 };
 
 router.use((req, res, next) => {
-  if (req.session.userInfo && req.session.userInfo.email != "") {
-    res.locals.userInfo = req.session.userInfo;
-    next();
-  } else if (
-    req.url === "/logIn" ||
-    req.url === "/signUp" ||
-    req.url === "/howto" ||
-    req.url === "/aboutUs" ||
-    req.url === "/"
-  ) {
-    next();
-  } else {
-    res.redirect("/");
-  }
+    if (req.session.userInfo && req.session.userInfo.email != "") {
+        res.locals.userInfo = req.session.userInfo;
+        next();
+    } else if (
+        req.url === "/logIn" ||
+        req.url === "/signUp" ||
+        req.url === "/howto" ||
+        req.url === "/aboutUs" ||
+        req.url === "/"
+    ) {
+        next();
+    } else {
+        res.redirect("/");
+    }
 });
 
 router.get(["/", "/howto"], (req, res, next) => {
-  res.render("howto");
+    res.render("howto");
 });
 
 router.get("/dashboard", (req, res, next) => {
-  res.render("dashboard");
+    res.render("dashboard");
 });
 
 router.get("/aboutUs", (req, res, next) => {
-  res.render("aboutUs");
+    res.render("aboutUs");
 });
 
 router.post("/signUp", (req, res) => {
-  loginDataAuth
-    .registerUser(req.body)
-    .then(result => {
-      console.log(result);
-      res.redirect("/");
-    })
-    .catch(err => {
-      console.log(err);
-      res.redirect("/");
-    });
+    loginDataAuth
+        .registerUser(req.body)
+        .then(result => {
+            console.log(result);
+            res.redirect("/");
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect("/");
+        });
 });
 
 router.post("/logIn", (req, res) => {
-  loginDataAuth
-    .checkUser(req.body)
-    .then(result => {
-      req.session.userInfo = result.userInfo;
-      res.redirect("/dashboard");
-    })
-    .catch(err => {
-      res.redirect("/");
-    });
+    loginDataAuth
+        .checkUser(req.body)
+        .then(result => {
+            req.session.userInfo = result.userInfo;
+            res.redirect("/dashboard");
+        })
+        .catch(err => {
+            res.redirect("/");
+        });
 });
 
 router.get("/logOut", (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/");
-    }
-  });
+    req.session.destroy(err => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
 module.exports = router;
