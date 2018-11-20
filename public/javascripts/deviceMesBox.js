14dae26e2dde48d1236b75a5866de868330c8e0b
Garrik-Liu <garrikliu@Gmail.com>
2018-11-19 22:47:32 -0500

    update

--- public/javascripts/deviceMesBox.js
+++ public/javascripts/deviceMesBox.js
@@ -1,35 +1,32 @@
 $(document).ready(function() {
-  var socket = io();
+    var socket = io();
 
-  $("#imgShowBoxId .closeBtn:first").click(function() {
-    $("#imgShowBoxId")
-      .toggleClass("imgShowBox--close")
-      .toggleClass("imgShowBox");
-  });
+    $("#imgShowBoxId .closeBtn:first").click(function() {
+        $("#imgShowBoxId")
+            .toggleClass("imgShowBox--close")
+            .toggleClass("imgShowBox");
+    });
 
-  socket.on("reload images", function(entities) {
-    loadImages(entities);
-    if ($("#deviceMesBox").css("width") == "0px") {
-      $("#deviceBtnMesCount").css("opacity", 1);
-      let num = Number($("#deviceBtnMesCount .mesCount__number:first").text());
-      $("#deviceBtnMesCount .mesCount__number:first").html(++num);
-    }
-  });
+    socket.on("reload images", function(entities) {
+        loadImages(entities);
+        if ($("#deviceMesBox").css("width") == "0px") {
+            $("#deviceBtnMesCount").css("opacity", 1);
+            let num = Number($("#deviceBtnMesCount .mesCount__number:first").text());
+            $("#deviceBtnMesCount .mesCount__number:first").html(++num);
+        }
+    });
 
-  socket.on("load images", function(entities) {
-    loadImages(entities);
-  });
+    socket.on("load images", function(entities) {
+        loadImages(entities);
+    });
 
-  function loadImages(entities) {
-    var content = "";
+    function loadImages(entities) {
+        var content = "";
 
-    entities.forEach(e => {
-      var eTime = new Date(e.time);
+        entities.forEach(e => {
+            var eTime = new Date(e.time);
 
-      // var imgTimeStr = eTime.getFullYear() + '-' + (eTime.getMonth() + 1) + '-' + eTime.getDate() + ' ' +
-      //     eTime.getHours() + ':' + eTime.getMinutes() + ':' + eTime.getSeconds();
-
-      var imgElement = `
+            var imgElement = `
                 <div class="deviceMesBox__imgElement">
                     <span class="deviceMesBox__imgTime">
                       ${eTime.toLocaleString()}
@@ -38,29 +35,29 @@ $(document).ready(function() {
                 </div>
             `;
 
-      content += imgElement;
-    });
+            content += imgElement;
+        });
 
-    $(".deviceMesBox__mesWindow:first").html(content);
+        $(".deviceMesBox__mesWindow:first").html(content);
 
-    ImageEventListener(".deviceMesBox__imgElement", "#imgShowBoxId");
-  }
+        ImageEventListener(".deviceMesBox__imgElement", "#imgShowBoxId");
+    }
 
-  function ImageEventListener(imgEleClass, imgShowId) {
-    $(imgEleClass).each(function(index) {
-      $(this).click(function() {
-        $(imgShowId)
-          .toggleClass("imgShowBox--close")
-          .toggleClass("imgShowBox");
-        $(imgShowId)
-          .children("img:first")
-          .attr(
-            "src",
-            $(this)
-              .children("img:first")
-              .attr("src")
-          );
-      });
-    });
-  }
+    function ImageEventListener(imgEleClass, imgShowId) {
+        $(imgEleClass).each(function(index) {
+            $(this).click(function() {
+                $(imgShowId)
+                    .toggleClass("imgShowBox--close")
+                    .toggleClass("imgShowBox");
+                $(imgShowId)
+                    .children("img:first")
+                    .attr(
+                        "src",
+                        $(this)
+                        .children("img:first")
+                        .attr("src")
+                    );
+            });
+        });
+    }
 });
