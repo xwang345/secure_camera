b554fcb8b3bf9a62ef5d50708de17dd6a1748047
Garrik-Liu <garrikliu@Gmail.com>
2018-11-21 22:41:40 -0500

    update

--- routes/trust.js
+++ routes/trust.js
@@ -12,32 +12,38 @@ router.setSocketIo = function(socket, io) {
 };
 
 // add a image to google cloud storage
-router.post("/addFace", myImages.multer.single("image"), myImages.sendUploadToGCS, (req, res, next) => {
-    let data = req.body;
+router.post(
+    "/addFace",
+    myImages.multer.single("image"),
+    myImages.sendUploadToGCS,
+    (req, res, next) => {
 
-    if (req.file && req.file.cloudStoragePublicUrl) {
-        data.imageUrl = req.file.cloudStoragePublicUrl;
-    }
+        let data = req.body;
 
-    // Save the data to the database.
-    modelDatastore.create(KIND, data, (err, savedData) => {
-        if (err) {
-            next(err);
-            return;
+        if (req.file && req.file.cloudStoragePublicUrl) {
+            console.log(123);
+            data.imageUrl = req.file.cloudStoragePublicUrl;
         }
 
-        modelDatastore.list(KIND, (err, entities) => {
+        // Save the data to the database.
+        modelDatastore.create(KIND, data, (err, savedData) => {
             if (err) {
                 next(err);
                 return;
             }
 
-            console.log(entities)
+            modelDatastore.list(KIND, (err, entities) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
 
-            res.redirect('/trustList');
+                console.log(entities)
+
+                res.redirect('/trustList');
+            });
         });
     });
-});
 
 router.get("/loadAll", (req, res, next) => {
     modelDatastore.list(KIND, (err, entities) => {
