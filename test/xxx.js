9be20bc34eebec408c8ec694369139f2053aad88
Garrik-Liu <garrikliu@Gmail.com>
2018-11-22 04:38:15 -0500

    update

--- test/xxx.js
+++ test/xxx.js
@@ -1,9 +1,8 @@
 const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
 
 loadImage('https://storage.googleapis.com/security-camera-bucket/15428786005781111.jpg').then((image) => {
+    const canvas = createCanvas(200, 200)
+    const ctx = canvas.getContext('2d')
     ctx.drawImage(image, 50, 0, 70, 70)
-
-    console.log('<img src="' + canvas.toDataURL() + '" />')
+    console.log(canvas.toDataURL())
 })
