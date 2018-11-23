892e84339f894513bd595f864d6d07ecabdda5e2
Garrik-Liu <garrikliu@Gmail.com>
2018-11-22 22:15:18 -0500

    update

--- public/javascripts/trustList.js
+++ public/javascripts/trustList.js
@@ -2,10 +2,10 @@ $('#faceUpload').imageupload(null, function() {
 
     let $image = $('#image');
 
-    var uploadedImage = new Image();
-    let previewCanvas = $('#faceUploadCropPreview');
-    let ctx = previewCanvas.getContext('2d');
-    uploadedImage.src = $image.attr('src');
+    // var uploadedImage = new Image();
+    // let previewCanvas = $('#faceUploadCropPreview');
+    // let ctx = previewCanvas.getContext('2d');
+    // uploadedImage.src = $image.attr('src');
 
     $image.cropper({
         aspectRatio: 4 / 3,
@@ -15,9 +15,9 @@ $('#faceUpload').imageupload(null, function() {
             let width = event.detail.width;
             let height = event.detail.height;
 
-            previewCanvas.width = uploadedImage.width;
-            previewCanvas.height = uploadedImage.height;
-            ctx.drawImage(uploadedImage, x, y, width, height, 0, 0, width, height);
+            // previewCanvas.width = uploadedImage.width;
+            // previewCanvas.height = uploadedImage.height;
+            // ctx.drawImage(uploadedImage, x, y, width, height, 0, 0, width, height);
         }
     });
 
