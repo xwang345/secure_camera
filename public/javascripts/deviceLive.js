b393cc79385f7a0be272ebf8bf0c109a40e71a3e
Garrik-Liu <garrikliu@Gmail.com>
2018-11-24 12:02:35 -0500

    update

--- public/javascripts/deviceLive.js
+++ public/javascripts/deviceLive.js
@@ -1,10 +1,7 @@
-$(document).ready(function() {
-    var canvas = document.getElementById("video-canvas");
-    var url = "ws://35.243.158.28:3000";
-    new JSMpeg.Player(url, { canvas: canvas });
+var videoCanvas = document.getElementById("video-canvas");
+var url = "ws://35.243.158.28:3000";
+new JSMpeg.Player(url, { canvas: videoCanvas });
 
-    $('#deviceSwitch').on('click', function() {
-        console.log('123')
-        socket.emit('toggle device')
-    })
-});
+$('#deviceSwitch').on('click', function() {
+    socket.emit('toggle device')
+})
