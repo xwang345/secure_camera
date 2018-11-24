e64243adb5e21476562069cf4c6b29c7721de18a
Garrik-Liu <garrikliu@Gmail.com>
2018-11-24 12:19:02 -0500

    update

--- public/javascripts/deviceLive.js
+++ public/javascripts/deviceLive.js
@@ -3,5 +3,13 @@ var url = "ws://35.243.158.28:3000";
 new JSMpeg.Player(url, { canvas: videoCanvas });
 
 $('#deviceSwitch').on('click', function() {
-    socket.emit('toggle device')
+    socket.emit('toggle device');
+})
+
+socket.on('update device status', function(data) {
+    if (data === 'off') {
+        $('#deviceSwitch').html('TURN OFF');
+    } else if (data === 'on') {
+        $('#deviceSwitch').html('TURN ON');
+    }
 })
