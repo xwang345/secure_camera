11a9d9d03b4c52fd84ac3cf3c31de0e2b59f5fb0
Garrik-Liu <garrikliu@Gmail.com>
2018-11-26 10:42:19 -0500

    update

--- public/javascripts/deviceMesBox.js
+++ public/javascripts/deviceMesBox.js
@@ -73,7 +73,7 @@ function ImageEventListener(imgEleClass, imgShowId, imgShowId_ControlPanel) {
                             if (similarity > 75) {
                                 let oldHtml = $('#imgShowBoxFaceDetectPanel').html();
                                 let newHtml = oldHtml + `
-                                <div id="imgShowBoxFaceDetectCard + ${element.name} + ${index}" class="card imgShowBox__faceDetectCard" style="width: 18rem;">
+                                <div id="imgShowBoxFaceDetectCard${element.name}${index}" class="card imgShowBox__faceDetectCard" style="width: 18rem;">
                                     <div class="card-body">
                                         <h5 class="card-title">${element.name}</h5>
                                         <p class="card-text">${element.description}</p>
@@ -81,7 +81,7 @@ function ImageEventListener(imgEleClass, imgShowId, imgShowId_ControlPanel) {
                                 </div>
                                 `;
                                 $('#imgShowBoxFaceDetectPanel').html(newHtml)
-                                faceDetectCardEventListener(`#imgShowBoxFaceDetectCard + ${element.name} + ${index}`, result);
+                                faceDetectCardEventListener(`#imgShowBoxFaceDetectCard${element.name}${index}`, result);
                             }
 
                         })
