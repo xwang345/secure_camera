1ea72ce63ff3a031aa1c09229051d54cc6599607
Garrik-Liu <garrikliu@Gmail.com>
2018-11-26 03:31:47 -0500

    update

--- public/javascripts/deviceMesBox.js
+++ public/javascripts/deviceMesBox.js
@@ -54,13 +54,17 @@ function ImageEventListener(imgEleClass, imgShowId, imgShowId_ControlPanel) {
                 .children("img:first")
                 .attr("src", imgUrl);
 
-            socket.emit('request trustFaces');
-            socket.on('get trustFaces', function(faceList) {
-                faceList.forEach((element) => {
-                    compareFaces(element.url, imgUrl, function(result) {
-                        console.log(result)
-                    })
-                })
+            // socket.emit('request trustFaces');
+            // socket.on('get trustFaces', function(faceList) {
+            //     faceList.forEach((element) => {
+            //         compareFaces(element.url, imgUrl, function(result) {
+            //             console.log(result)
+            //         })
+            //     })
+            // })
+
+            detectFaces(imgUrl, function(data) {
+                console.log(data)
             })
 
 
