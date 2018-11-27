4d5dd2a8f53a68c5ff1006d5c2bbae457d4886ae
Garrik-Liu <garrikliu@Gmail.com>
2018-11-27 09:19:13 -0500

    update

--- public/javascripts/deviceMesBox.js
+++ public/javascripts/deviceMesBox.js
@@ -104,9 +104,10 @@ function ImageEventListener(imgEleClass, imgShowId, imgShowBox__imgContainer) {
                                     "";
                                 let similarity = firstFace.Similarity;
                                 if (similarity > 75) {
-                                    if (similarity >= trustFacesObj.faceInSnapshot[firstFaceId].Similarity) {
-                                        trustFacesObj.faceInSnapshot[firstFaceId] = firstFace;
-                                    }
+                                    console.log(trustFacesObj.faceInSnapshot[firstFaceId])
+                                        // if (similarity >= trustFacesObj.faceInSnapshot[firstFaceId].Similarity) {
+                                        //     trustFacesObj.faceInSnapshot[firstFaceId] = firstFace;
+                                        // }
                                     let oldHtml = $('#imgShowBoxFaceDetectPanel').html();
                                     let newHtml = oldHtml + `
                                         <div id="imgShowBoxFaceDetectCard_$${element.url}" class="card bg-success text-white imgShowBox__faceDetectCard" style="width: 18rem;">
